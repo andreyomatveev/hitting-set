02006e55c251c2d82010ccf43143682ec18f4669
Andrey O. Matveev <106903208+andreyomatveev@users.noreply.github.com>
2022-07-29 10:31:28 +0500

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -8,4 +8,4 @@ import qualified Data.Set                          as St
 
 main :: IO ()
 main = 
-    putStrLn ("\n" ++ "findLexicographicallyMinimalBlockingSetOfMinimumCardinality ( St.fromList [ ISt.fromList [-3,0], ISt.fromList [6,3], ISt.fromList [3,-2,0] ] )    returns   "  ++ show (findLexicographicallyMinimalBlockingSetOfMinimumCardinality ( St.fromList [ ISt.fromList [-3,0], ISt.fromList [6,3], ISt.fromList [3,-2,0] ] )) ++ "\n")
+    putStrLn ("\n" ++ "findLexicographicallyMinimalBlockingSetOfMinimumCardinality ( St.fromList [ ISt.fromList [-3,1], ISt.fromList [6,1], ISt.fromList [3,-2,0] ] )    returns   "  ++ show (findLexicographicallyMinimalBlockingSetOfMinimumCardinality ( St.fromList [ ISt.fromList [-3,1], ISt.fromList [6,1], ISt.fromList [3,-2,0] ] )) ++ "\n")
